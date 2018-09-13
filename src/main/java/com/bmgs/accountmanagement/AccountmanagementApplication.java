88ef384d7308b4c2fc2b3e4cc25c06930d921577
Yunus Emre KURT <yekurt@borusan.com>
2018-09-13 16:11:33 +0300

    initial

---
+++ src/main/java/com/bmgs/accountmanagement/AccountmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.bmgs.accountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountmanagementApplication.class, args);
+	}
+}
